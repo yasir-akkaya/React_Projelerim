81406e2d5f22d444f6d7951833af84125d6b2b22
yasir-akkaya <yasirakkaya@outlook.com>
2023-11-08 00:03:39 +0300

    E-Ticaret
    e-ticaret sitesi tasarımı yapıldı

---
+++ 07.11.2023/uygulama/src/App.js
@@ -0,0 +1,85 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <img className="logo" src="https://www.webtures.com/tr/blog/wp-content/uploads/4-3-e1471605757167-281x300.png" alt="" />
+        <ul className="navbar-menu">
+          <li className="navbar-item"><a href="#">Anasayfa</a></li>
+          <li className="navbar-item"><a href="#">Hakkımızda</a></li>
+          <li className="navbar-item"><a href="#">Kategoriler</a></li>
+          <li className="navbar-item"><a href="#">İletişim</a></li>
+        </ul>
+      </div>
+      <div className="sidebar">
+        <ul className="sidebar-menu">
+          <li className="sidebar-item"><a href="#">Giyim</a></li>
+          <li className="sidebar-item"><a href="#">Dekorasyon</a></li>
+          <li className="sidebar-item"><a href="#">Tablet</a></li>
+          <li className="sidebar-item"><a href="#">Bilgisayar</a></li>
+          <li className="sidebar-item"><a href="#">Telefon</a></li>
+          <li className="sidebar-item"><a href="#">Kulaklık</a></li>
+          <li className="sidebar-item"><a href="#">Klima</a></li>
+          <li className="sidebar-item"><a href="#">Klavye</a></li>
+          <li className="sidebar-item"><a href="#">Mouse</a></li>
+          <li className="sidebar-item"><a href="#">Mousepad</a></li>
+          <li className="sidebar-item"><a href="#">VR</a></li>
+          <li className="sidebar-item"><a href="#">PS</a></li>
+          <li className="sidebar-item"><a href="#">Xbox</a></li>
+          <li className="sidebar-item"><a href="#">Oyuncak</a></li>
+          <li className="sidebar-item"><a href="#">Hediyelik</a></li>
+          <li className="sidebar-item"><a href="#">Süpürge</a></li>
+          <li className="sidebar-item"><a href="#">Ütü</a></li>
+          <li className="sidebar-item"><a href="#">Ütü Masası</a></li>
+          <li className="sidebar-item"><a href="#">Ayakkabı</a></li>
+          <li className="sidebar-item"><a href="#">Top</a></li>
+          <li className="sidebar-item"><a href="#">Tabak</a></li>
+        </ul>
+      </div>
+      <div className="content">
+        <div className="content-box">
+          <img src="https://images.squarespace-cdn.com/content/v1/58412fc9b3db2b11ba9398df/1571320976089-M6JWOXX4Q7XGTEKE9MGL/studyo-daire-dekorasyonu" alt="" />
+          <h4>Lorem, ipsum.</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <a href="#">Detaylar...</a>
+        </div>
+        <div className="content-box">
+          <img src="https://images.squarespace-cdn.com/content/v1/58412fc9b3db2b11ba9398df/1571320976089-M6JWOXX4Q7XGTEKE9MGL/studyo-daire-dekorasyonu" alt="" />
+          <h4>Lorem, ipsum.</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <a href="#">Detaylar...</a>
+        </div>
+        <div className="content-box">
+          <img src="https://images.squarespace-cdn.com/content/v1/58412fc9b3db2b11ba9398df/1571320976089-M6JWOXX4Q7XGTEKE9MGL/studyo-daire-dekorasyonu" alt="" />
+          <h4>Lorem, ipsum.</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <a href="#">Detaylar...</a>
+        </div>
+        <div className="content-box">
+          <img src="https://images.squarespace-cdn.com/content/v1/58412fc9b3db2b11ba9398df/1571320976089-M6JWOXX4Q7XGTEKE9MGL/studyo-daire-dekorasyonu" alt="" />
+          <h4>Lorem, ipsum.</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <a href="#">Detaylar...</a>
+        </div>
+        <div className="content-box">
+          <img src="https://images.squarespace-cdn.com/content/v1/58412fc9b3db2b11ba9398df/1571320976089-M6JWOXX4Q7XGTEKE9MGL/studyo-daire-dekorasyonu" alt="" />
+          <h4>Lorem, ipsum.</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <a href="#">Detaylar...</a>
+        </div>
+        <div className="content-box">
+          <img src="https://images.squarespace-cdn.com/content/v1/58412fc9b3db2b11ba9398df/1571320976089-M6JWOXX4Q7XGTEKE9MGL/studyo-daire-dekorasyonu" alt="" />
+          <h4>Lorem, ipsum.</h4>
+          <p>Lorem ipsum dolor sit</p>
+          <a href="#">Detaylar...</a>
+        </div>
+      </div>
+    </div>
+
+
+  );
+}
+
+export default App;
