92c3825aa299d89c406fc1c904ed9dffb6025107
yasir-akkaya <yasirakkaya@outlook.com>
2023-11-08 22:58:00 +0300

    08.11.2023 Crud List Ödevi
    Bu uygulamada bir blog sitesi tasarımının taslağı oluşturuldu. Site; blog ekleme, blog düzenleme ve blog silme özelliikleriyle oluşturulup, genel düzenlemeler yapılmıştır.

---
+++ 08.11.2023-Liste Crud Odevi/list_crud_app/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import CrudListing from './CrudListing';
+function App() {
+  return (
+    <div className="App">
+      
+        <div className="container text-center">
+          <div className="row">
+            
+            <div className="col">
+              <CrudListing/>
+            </div>
+            
+          </div>
+        </div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ 08.11.2023-Liste Crud Odevi/list_crud_app/src/CrudListing.js
@@ -0,0 +1,64 @@
+import logo from './logo.svg';
+import './App.css';
+import { React, useState } from "react";
+
+
+function CrudListing() {
+    const [titleInput, setTitleInput] = useState("");
+    const [textInput, setTextInput] = useState("");
+    const [blogList, setBlogList] = useState([]);
+    const [editingIndex, setEditingIndex] = useState(null);
+
+    const ProductFunction = () => {
+        if (titleInput && textInput) {
+            if (editingIndex !== null) {
+                const updatedTasks = [...blogList];
+                updatedTasks[editingIndex] = { titleInput, textInput };
+                setBlogList(updatedTasks);
+                setEditingIndex(null);
+            }
+
+            else {
+                setBlogList([...blogList, { titleInput, textInput }]);
+
+            }
+            setTitleInput("");
+            setTextInput("");
+
+        }
+    };
+    const EditFunction = (index) => {
+        setEditingIndex(index);
+        const taskToEdit = blogList[index];
+        setTitleInput(taskToEdit.titleInput);
+        setTextInput(taskToEdit.textInput);
+    };
+
+
+    return (
+        <div className="blogs">
+            <h3>Blog Listesi: </h3>
+            <div className="form-group mx-sm-2 p-3 border border-dark rounded">
+                <input type="text" value={titleInput} className="form-control mb-3" placeholder="Blog Başlığı" onChange={(e) => setTitleInput(e.target.value)} />
+                <textarea value={textInput} className="form-control mb-3" placeholder="Blog Yazısı" rows="8" onChange={(e) => setTextInput(e.target.value)} />
+                <a href='#' onClick={ProductFunction} className="btn btn-success" type="button">Blog Yazısı Ekle</a>
+            </div>
+            <div className='rounded mt-3'>
+                <ul className="list list-group">
+                    {blogList.map((blog, index) => (
+                        <li className='list-group-item' key={index}>
+                            <h3>{blog.titleInput}</h3>
+                            <p>{blog.textInput}</p>
+                            <a href='#' onClick={() => EditFunction(index)} className="btn btn-warning ms-2" type="button">Blog Düzenle</a>
+                            <a href='#' onClick={() => setBlogList(blogList.filter((item) => item != blog))} className="btn btn-danger ms-2" type="button">Blog Sil</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+
+    );
+}
+
+
+export default CrudListing;
